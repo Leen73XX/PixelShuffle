482f981b0d9a7f70205fe7e30f0d5744ac9d7455
Leen73XX <leen.mej@gmail.com>
2026-08-29 23:28:04 +0300

    check

---
+++ image_divider/Preview Content/Extension/color.swift
@@ -0,0 +1,8 @@
+//
+//  color.swift
+//  image_divider
+//
+//  Created by Leen Almejarri on 16/03/1448 AH.
+//
+
+import Foundation
---
+++ image_divider/Preview Content/Model/model.swift
@@ -0,0 +1,8 @@
+//
+//  model.swift
+//  image_divider
+//
+//  Created by Leen Almejarri on 16/03/1448 AH.
+//
+
+import Foundation
