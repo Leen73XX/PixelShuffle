9348c29774ba8c928342a7503d451f20fa9d0e89
Leen73XX <leen.mej@gmail.com>
2026-08-29 23:02:24 +0300

    Initial Commit

---
+++ image_divider/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  image_divider
+//
+//  Created by Leen Almejarri on 16/03/1448 AH.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ image_divider/image_dividerApp.swift
@@ -0,0 +1,17 @@
+//
+//  image_dividerApp.swift
+//  image_divider
+//
+//  Created by Leen Almejarri on 16/03/1448 AH.
+//
+
+import SwiftUI
+
+@main
+struct image_dividerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
